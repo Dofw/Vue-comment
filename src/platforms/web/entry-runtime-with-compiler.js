533cb51de9aa9e1916b62a5ba5368de09c4622d4
dofw <1686661978@qq.com>
2021-04-29 12:20:34 +0800

    注释：入口runtime-with-compiler

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -1,4 +1,19 @@
 /* @flow */
+/*
+  注释： 6条
+
+  功能： 
+  1. Vue.prototype.$mount 改写 ，将template 转化成 render函数。  引入'./compiler/index'。 最后在执行保存原来的 mount进行挂载。
+  
+  核心引入：
+  import Vue from './runtime/index' 依赖Vue
+  import { compileToFunctions } from './compiler/index'  编译模板
+
+  导出说明：
+  此时的Vue.prototype.$mount 编译template，编译后， 执行原始的mount方法。
+*/
+
+
 
 import config from 'core/config'
 import { warn, cached } from 'core/util/index'
@@ -9,16 +24,23 @@ import { query } from './util/index'
 import { compileToFunctions } from './compiler/index'
 import { shouldDecodeNewlines, shouldDecodeNewlinesForHref } from './util/compat'
 
+// 1.cached在 shared/util里， 工厂模式
 const idToTemplate = cached(id => {
   const el = query(id)
   return el && el.innerHTML
 })
 
+// 2. 将vue的$mount，保存。 
 const mount = Vue.prototype.$mount
+
+// 3. 改写 $mount 是对 template 转化 render， compile后再调用 mount。
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
 ): Component {
+
+// 4.vue,模板渲染优先级： render函数 -> template(  1.配置字符串，  2. 内联模板(组件行间添加 inline-template属性） 3.X-template（定义script标签 type='text/x-template) ,然后通过id引入到选项中 ） -> 没有在看el
+
   el = el && query(el)
 
   /* istanbul ignore if */
@@ -35,6 +57,8 @@ Vue.prototype.$mount = function (
     let template = options.template
     if (template) {
       if (typeof template === 'string') {
+
+        // 5. X-template
         if (template.charAt(0) === '#') {
           template = idToTemplate(template)
           /* istanbul ignore if */
@@ -45,7 +69,8 @@ Vue.prototype.$mount = function (
             )
           }
         }
-      } else if (template.nodeType) {
+
+      } else if (template.nodeType) { // 难道是vue文件
         template = template.innerHTML
       } else {
         if (process.env.NODE_ENV !== 'production') {
@@ -56,6 +81,9 @@ Vue.prototype.$mount = function (
     } else if (el) {
       template = getOuterHTML(el)
     }
+
+
+    // 6. template 变成 render
     if (template) {
       /* istanbul ignore if */
       if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
